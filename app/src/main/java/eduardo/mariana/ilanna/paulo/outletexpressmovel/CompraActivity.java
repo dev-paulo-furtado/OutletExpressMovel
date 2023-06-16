04500e9864d2910def240ed8025758bf46e34359
20211TIIMI0044 <eduardo.olmosantana@gmail.com>
2023-06-16 10:47:21 -0300

    Criação da tela de compra

--- app/src/main/java/eduardo/mariana/ilanna/paulo/outletexpressmovel/CompraActivity.java
+++ app/src/main/java/eduardo/mariana/ilanna/paulo/outletexpressmovel/CompraActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_loja extends AppCompatActivity {
+public class CompraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loja);
+        setContentView(R.layout.activity_compra);
     }
 }
