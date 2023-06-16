f7d31b366de0eecb7370beab29312a266dea28d3
20211TIIMI0249 <paulocezarrf@hotmail.com>
2023-06-16 11:07:01 -0300

    Layout HomeActivity e implementacao dos fragmentos

--- app/src/main/java/eduardo/mariana/ilanna/paulo/outletexpressmovel/HomeActivity.java
+++ app/src/main/java/eduardo/mariana/ilanna/paulo/outletexpressmovel/HomeActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FragmentosActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragmentos);
+        setContentView(R.layout.activity_home);
     }
 }
